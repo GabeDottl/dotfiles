308cd24f5aadc935416008efbd3ee4f34a1d3462
gabedottl@gmail.com <gabedottl@gmail.com>
2019-04-21 22:00:26 -0700

    x

--- gen_soft_links.sh
+++ gen_soft_links.sh
@@ -1,7 +1,5 @@
-#ln -s $DOTFILES/config ~/.config
 ln -s $DOTFILES/ssh ~/.ssh
 ln -s $DOTFILES/public/zshrc ~/.zshrc
-#ln -s $DOTFILES/zsh_history ~/.zsh_history Un-necessary
 ln -s $DOTFILES/config/vimrc ~/.vimrc
 ln -s $DOTFILES/config/gitconfig ~/.gitconfig
 ln -s $DOTFILES/public/gitignore_global ~/.gitignore_global
