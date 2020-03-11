bec7b4897e9c81ccad69b4b3a2540d8adc881563
Gabe Dottl <gabedottl@gmail.com>
2020-03-11 07:31:46 -0700

    Moved in p10k config.

--- gen_soft_links.sh
+++ gen_soft_links.sh
@@ -18,3 +18,4 @@ ln -s $DOTFILES/jupyter ~/.jupyter
 mkdir ~/.config
 ln -s $DOTFILES/config/yapf ~/,config/yapf
 $DOTFILES/config/$HOSTNAME/gen_soft_links.sh
+ln -s /home/gabe/dotfiles/public/p10k.zsh ~/.p10k.zsh
