601e485be784cf0bd6ee3e8115efcacd3af0ea5c
Gabe Dottl <gabedottl@gmail.com>
2020-03-15 09:37:29 -0700

    Moved logic into config - also, smarter '/home/gabe/dotfiles' logic.

--- gen_soft_links.sh
+++ gen_soft_links.sh
@@ -1,21 +1,14 @@
-mkdir ~/.ptpython
-DOTFILES=$HOME/dotfiles
+DOTFILES=$(dirname $0)
+#DOTFILES=$HOME/dotfiles
 
 ln -s $DOTFILES/ssh ~/.ssh
 mv ~/.zshrc ~/.zshrc.old
 ln -s $DOTFILES/public/zshrc ~/.zshrc
-ln -s $DOTFILES/config/vimrc ~/.vimrc
-ln -s $DOTFILES/config/gitconfig ~/.gitconfig
 ln -s $DOTFILES/public/gitignore_global ~/.gitignore_global
-ln -s $DOTFILES/oh-my-zsh ~/.oh-my-zsh
-ln -s $DOTFILES/pythonrc ~/.pythonrc
-ln -s $DOTFILES/config/ptpython $HOME/Library/Application Support/ptpython
-#ln -s $DOTFILES/history/$HOSTNAME/ptpython_history ~/.ptpython/history
-ln -s $DOTFILES/config/ptpython/config.py ~/.ptpython/config.py
 ln -s $DOTFILES/public/tmux.conf ~/.tmux.conf
-ln -s $DOTFILES/config/$HOSTNAME/tmux.conf ~/tmux_extra
 ln -s $DOTFILES/jupyter ~/.jupyter
-mkdir ~/.config
-ln -s $DOTFILES/config/yapf ~/,config/yapf
+ln -s $DOTFILES/oh-my-zsh ~/.oh-my-zsh
+$DOTFILES/config/gen_soft_links.sh
 $DOTFILES/config/$HOSTNAME/gen_soft_links.sh
-ln -s $DOTFILES/config/$HOSTNAME/p10k.zsh ~/.p10k.zsh
+
+
